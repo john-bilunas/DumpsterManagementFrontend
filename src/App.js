dbadac69300dc58bd91074e4e96bd185a9797ec1
John Bilunas <johnnyb7184@gmail.com>
2025-02-24 16:10:16 -0500

    Created basic routing for the core pages - Home, Customers, Rentals, Inventory and pricing.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import InventoryAndPricing from './components/InventoryAndPricing';
+import Customers from './components/Customers';
+import Rentals from './components/Rentals';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+    <Header/>
+    <main>
+        {/* Render components in the main */}
+      <Routes>
+        <Route path= "/" element = {<Home/>}/>
+        <Route path= "/Inventory" element= { <InventoryAndPricing/>}/>
+        <Route path= "Customers" element = {<Customers/>}/>
+        <Route path= "/Rentals" element= {<Rentals/>}/>
+        
+      </Routes>
+    </main>
+    <Footer/>
+      
+    </>
   );
 }
 
---
+++ src/components/InventoryAndPricing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InventoryAndPricing = () => {
+  return (
+    <div>InventoryAndPricing</div>
+  )
+}
+
+export default InventoryAndPricing
